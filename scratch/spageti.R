bf696851d02645ab28ac42ad204561de2cc73d12
Lucian Scher <lucian@bren.ucsb.edu>
2025-08-28 13:40:57 -0700

    Peer Review

--- scratch/spageti.R
+++ scratch/spageti.R
@@ -2,6 +2,8 @@
 # Lucian Scher
 # lucian@ucsb.edu
 
+rm(list = ls())
+
 # Libraries
 library(here)
 library(janitor)
@@ -35,16 +37,11 @@ BQ12 <- full_join(BQ1, BQ2)
 BQ3PRM <- full_join(BQ3, PRM)
 
 fig3data <- full_join(BQ12, BQ3PRM) |> 
-  filter(year(sample_date) <= "1994") # Change dates to Fig 3 range
+  filter(year(sample_date) >= "1988" & year(sample_date) <= "1994")# Change dates to Fig 3 range
 
-ymd(fig3data$sample_date)
 
-# Long version of data with avg to use facet wrap removing original data
+# ymd(fig3data$sample_date)
 
-fig3data_long <- fig3data |>
-  pivot_longer(cols = c(k, no3_n, mg, ca, nh4_n),
-               names_to = "nutrient",
-               values_to = "value") 
  # select(-nutrient, -value)
 
 # Write function for finding rolling means
@@ -61,23 +58,147 @@ moving_average <- function(focal_date, dates, conc, win_size_wks) {
   return(result)
 }
 
+# Create separate data frames for each nutrient bc I dont know how else to do it
+
+# Tried splitting I don't think it worked :(
+#fig3data_long_split <- split(fig3data_long, fig3data_long$nutrient)
+#fig3data_long_split_id <- split(fig3data_long, fig3data_long$sample_id)
+#View(fig3data_long_split[["ca"]])
+
+# Make new data frames that is just the nutrient, sample_date and sample_id
+
+k <- fig3data_long |> 
+  filter(nutrient == "k") |> 
+  select(-nutrient) |> 
+  mutate(sample_date = as.Date(sample_date))
+
+no3_n <- fig3data_long |> 
+  filter(nutrient == "no3_n")|> 
+  select(-nutrient) |>
+  mutate(sample_date = as.Date(sample_date))
+
+mg <- fig3data_long |> 
+  filter(nutrient == "mg")|> 
+  select(-nutrient) |>
+  mutate(sample_date = as.Date(sample_date))
+
+ca <- fig3data_long |> 
+  filter(nutrient == "ca")|> 
+  select(-nutrient) |>
+  mutate(sample_date = as.Date(sample_date))
+
+nh4_n <- fig3data_long |> 
+  filter(nutrient == "nh4_n")|> 
+  select(-nutrient) |>
+  mutate(sample_date = as.Date(sample_date))
+
 
 # Call Function
 source("R/rolling_mean.R")
-# Apply the function
 
-fig3data_long$roll_mean <- sapply(
-  fig3data_long$sample_date,
-  moving_average,
-  dates = fig3data_long$sample_date,
-  conc = fig3data_long$value,
-  win_size_wks = 9
-)
+# Apply the function for k
+
+rollmean_k <- k |>
+  group_by(sample_id) |>
+  mutate(
+    k_rollmean = sapply( # Applies function iterating through sample_date
+    sample_date,
+   function(fd) moving_average( # Tells function to focus on a focal date
+      focal_date = fd,
+      dates = sample_date,
+      conc = value,
+      win_size_wks = 9
+    )
+  )
+)|> 
+  select(-value)
 
 
-# Graphing lines to show each nutrient by year with individual y-axiss
 
-fig3plot <- ggplot(data = fig3data_long, 
+# Apply the function for no3_n
+
+rollmean_no3_n <- no3_n |>
+  group_by(sample_id) |>
+  mutate(
+    no3_n_rollmean = sapply(
+      sample_date,
+      function(fd) moving_average(
+        focal_date = fd,
+        dates = sample_date,
+        conc = value,
+        win_size_wks = 9
+      )
+    )
+  ) |> 
+  select(-value)
+
+# Apply the function for mg
+
+rollmean_mg <- mg |>
+  group_by(sample_id) |>
+  mutate(
+    mg_rollmean = sapply(
+      sample_date,
+      function(fd) moving_average(
+        focal_date = fd,
+        dates = sample_date,
+        conc = value,
+        win_size_wks = 9
+      )
+    )
+  )|> 
+  select(-value)
+
+
+# Apply the function for ca
+
+rollmean_ca <- ca |>
+  group_by(sample_id) |>
+  mutate(
+    ca_rollmean = sapply(
+      sample_date,
+      function(fd) moving_average(
+        focal_date = fd,
+        dates = sample_date,
+        conc = value,
+        win_size_wks = 9
+      )
+    )
+  )|> 
+  select(-value)
+
+
+# Apply the function for nh4_n
+
+rollmean_nh4_n <- nh4_n |>
+  group_by(sample_id) |>
+  mutate(
+    nh4_n_rollmean = sapply(
+      sample_date,
+      function(fd) moving_average(
+        focal_date = fd,
+        dates = sample_date,
+        conc = value,
+        win_size_wks = 9
+      )
+    )
+  )|> 
+  select(-value)
+
+# Regroup data for plotting also making it long for plotting
+
+rollmean_all <- rollmean_k |> 
+  full_join(rollmean_no3_n, by = c("sample_id", "sample_date")) |> 
+  full_join(rollmean_mg,    by = c("sample_id", "sample_date")) |> 
+  full_join(rollmean_ca,    by = c("sample_id", "sample_date")) |> 
+  full_join(rollmean_nh4_n, by = c("sample_id", "sample_date")) |> 
+  pivot_longer(cols = c(k_rollmean, no3_n_rollmean, mg_rollmean, ca_rollmean, nh4_n_rollmean),
+               names_to = "nutrient",
+               values_to = "value") 
+
+# Graphing lines to show each nutrient by year with individual y-axis
+
+fig3plot <- ggplot(data = rollmean_all, 
                    aes(x = sample_date,
                    y = value,
                    group = sample_id,
@@ -95,3 +216,4 @@ fig3plot
 # mutate(avg_mg = rollmean(mg, k = 9, align = "center", fill = NA, na.rm = TRUE)) |> 
 # mutate(avg_ca = rollmean(ca, k = 9, align = "center", fill = NA, na.rm = TRUE)) |> 
 # mutate(avg_nh4 = rollmean(nh4_n, k = 9, align = "center", fill = NA, na.rm = TRUE)) 
+
---
+++ self-assessment.md
@@ -0,0 +1,32 @@
+#Automate
+[ ] Running the entire analysis requires rendering one Quarto document
+Nope
+[ ] The analysis runs without errors
+Not yet
+[ ] The analysis produces the expected output
+Definetly not
+[ ] Data import/cleaning is handled in its own script(s)
+Actually yes kind of 
+#Organize
+[ ] Raw data is contained in its own folder
+Yessirski
+[ ] Intermediate outputs are created and saved to a separate folder from raw data
+uhhhhh maybe
+[ ] At least one piece of functionality has been refactored into a function in its own file
+Yuppers!
+#Document
+[ ] The repo has a README that explains where to find (1) data, (2) analysis script, (3) supporting code, and (4) outputs
+Work In Progress
+[ ] The README includes a flowchart and text explaining how the analysis works
+not yet
+[ ] The code is appropriately commented
+what exists of it sure lol
+[ ] Variable and function names are descriptive and follow a consistent naming convention
+I beleive so
+#Scale
+After cloning the repo on Workbench:
+
+[ ] Running the environment initialization script installs all required packages
+haha
+[ ] The analysis script runs without errors
+hahahahaha
